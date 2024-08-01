c2671d2fd85f5d01b2062456225f54ddd671e739
Mark <markporcarojr@gmail.com>
2024-08-01 11:49:51 -0400

    Added Credentials Provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { Span } from "next/dist/trace";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
